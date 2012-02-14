c1a24709418806760ff5812bb987b9add47313a6
Phil Tang <tangphillip@gmail.com>
2012-02-14 16:42:32 -0500

    Initial Commit

---
+++ EnduroTests/EnduroTests.h
@@ -0,0 +1,13 @@
+//
+//  EnduroTests.h
+//  EnduroTests
+//
+//  Created by Phillip Tang on 2/14/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface EnduroTests : SenTestCase
+
+@end
---
+++ EnduroTests/EnduroTests.m
@@ -0,0 +1,32 @@
+//
+//  EnduroTests.m
+//  EnduroTests
+//
+//  Created by Phillip Tang on 2/14/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "EnduroTests.h"
+
+@implementation EnduroTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in EnduroTests");
+}
+
+@end
